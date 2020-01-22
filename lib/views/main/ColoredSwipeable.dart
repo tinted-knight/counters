8158780ee683f53c9a6d0fdca03f22f077228069
tinted-knight <the.tinted.knight@gmail.com>
2020-01-22 17:53:26 +0300

    indev: Main screen > swipe to increase > animation:  - animate back to transparent when swipe counted  - red flash when swipe was not counted

---
+++ lib/views/main/ColoredSwipeable.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+typedef OnSwiped = Function();
+
+class ColoredSwipeable extends StatefulWidget {
+  const ColoredSwipeable({
+    Key key,
+    @required this.child,
+    @required this.onSwiped,
+    @required this.onTap,
+  }) : super(key: key);
+
+  final Widget child;
+  final OnSwiped onSwiped;
+  final GestureTapCallback onTap;
+
+  @override
+  _ColoredSwipeableState createState() => _ColoredSwipeableState();
+}
+
+class _ColoredSwipeableState extends State<ColoredSwipeable>
+    with SingleTickerProviderStateMixin {
+  Color color;
+
+  int _value = 255;
+
+  final _distanceLimit = 150;
+  final _velocityLimit = 500.0;
+
+  AnimationController _colorController;
+  Animation<Color> _currentAnimation;
+  Animation<Color> _greenAnimation;
+  Animation<Color> _redFlashAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    color = Color.fromARGB(50, 255, 255, 255);
+    _colorController = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _greenAnimation = ColorTween(
+      begin: Color.fromARGB(50, 255, 255, 255),
+      end: Color.fromARGB(50, 0, 255, 0),
+    ).animate(_colorController);
+
+    _redFlashAnimation = ColorTween(
+      begin: Color.fromARGB(50, 255, 0, 0),
+      end: Color.fromARGB(50, 255, 255, 255),
+    ).animate(_colorController);
+
+    _currentAnimation = _greenAnimation;
+  }
+
+  void _swipeNotCounted() {
+    _colorController.reverse();
+    _currentAnimation = _redFlashAnimation;
+    _colorController.reset();
+    _colorController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) _resetToGreen();
+    });
+    _colorController.forward();
+  }
+
+  void _resetToGreen() {
+    _colorController.reset();
+    _currentAnimation = _greenAnimation;
+  }
+
+  void _swipeCounted() {
+    widget.onSwiped();
+    _colorController.reverse();
+  }
+
+  void _dragUpdate(DragUpdateDetails details) {
+    final delta = (details.delta.dx * 2).floor();
+    // decreasing red and green until 0 but not less
+    final newValue = _value >= delta ? _value -= delta : _value = 0;
+    final target = 1 - newValue / 255;
+    _colorController.value = target;
+    setState(() {
+      _value = newValue;
+    });
+  }
+
+  void _dragEnd(DragEndDetails details) {
+    // if there is enough velocity in user's swipe
+    // or swipe distance was long enough
+    // then considering that swipe was successful
+    if (details.velocity.pixelsPerSecond.dx >= _velocityLimit ||
+        _value <= _distanceLimit) {
+      _swipeCounted();
+    } else {
+      _swipeNotCounted();
+    }
+    setState(() {
+      _value = 255;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        widget.onTap();
+      },
+      onHorizontalDragUpdate: _dragUpdate,
+      onHorizontalDragEnd: _dragEnd,
+      child: AnimatedBuilder(
+        animation: _currentAnimation,
+        builder: (context, child) {
+          return Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: Container(
+                    color: _currentAnimation.value,
+                    padding: EdgeInsets.all(16.0)),
+              ),
+              widget.child,
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/views/main/Counters.dart
+++ lib/views/main/Counters.dart
@@ -3,6 +3,7 @@ import 'package:counter/bloc/CounterListBloc.dart';
 import 'package:counter/model/CounterModel.dart';
 import 'package:flutter/material.dart';
 
+import 'ColoredSwipeable.dart';
 import 'counter_row/CounterRow.dart';
 
 class Counters extends StatelessWidget {
@@ -43,7 +44,7 @@ class Counters extends StatelessWidget {
     return ListView.builder(
       itemCount: items.length,
       itemBuilder: (ctx, index) {
-        return TestWidget(
+        return ColoredSwipeable(
           onTap: () => itemTap(items[index]),
           onSwiped: () => bloc.incrementCounter(items[index]),
           child: Column(
@@ -66,113 +67,4 @@ class Counters extends StatelessWidget {
         margin: EdgeInsets.only(left: 32.0, right: 32.0),
         color: Colors.black12,
       );
-}
-
-typedef OnSwiped = Function();
-
-class TestWidget extends StatefulWidget {
-  const TestWidget({
-    Key key,
-    @required this.child,
-    @required this.onSwiped,
-    @required this.onTap,
-  }) : super(key: key);
-
-  final Widget child;
-  final OnSwiped onSwiped;
-  final GestureTapCallback onTap;
-
-  @override
-  _TestWidgetState createState() => _TestWidgetState();
-}
-
-class _TestWidgetState extends State<TestWidget>
-    with SingleTickerProviderStateMixin {
-  Color color;
-
-  int _value = 255;
-
-  final _limit = 150;
-  final _velocity = 500.0;
-
-  AnimationController _colorController;
-  Animation<Color> _colorAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    color = Color.fromARGB(50, 255, 255, 255);
-    _colorController = AnimationController(
-      duration: Duration(milliseconds: 500),
-      vsync: this,
-    );
-    _colorAnimation = ColorTween(
-      end: Color.fromARGB(50, 0, 255, 0),
-      begin: Color.fromARGB(50, 255, 255, 255),
-    ).animate(_colorController);
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        widget.onTap();
-      },
-      onHorizontalDragUpdate: (details) {
-        final delta = (details.delta.dx * 2).floor();
-        // decreasing red and green until 0 but not less
-        final newValue = _value >= delta ? _value -= delta : _value = 0;
-        final target = 1 - newValue / 255;
-        _colorController.value = target;
-        setState(() {
-          _value = newValue;
-        });
-      },
-      onHorizontalDragEnd: (details) {
-        // if there is enough velocity in user's swipe
-        // or swipe distance was long enough
-        // then considering that swipe was successful
-        if (details.velocity.pixelsPerSecond.dx >= _velocity ||
-            _value <= _limit) {
-          widget.onSwiped();
-          setState(() {
-            _value = 255;
-          });
-          _colorController.reverse();
-        }
-      },
-      child: AnimatedBuilder(
-        animation: _colorAnimation,
-        builder: (context, child) {
-          return Stack(
-            children: <Widget>[
-              Positioned.fill(
-                child: Container(
-                    color: _colorAnimation.value,
-                    padding: EdgeInsets.all(16.0)),
-              ),
-              widget.child,
-            ],
-          );
-        },
-//        child: widget.child,
-      ),
-//      child: Stack(
-//        children: <Widget>[
-//          Positioned.fill(
-//            child: Container(
-////                color: color.withRed(_red).withGreen(_green).withBlue(_blue),
-//                color: _colorAnimation.value,
-//                padding: EdgeInsets.all(16.0)),
-//          ),
-//          widget.child,
-//        ],
-//      ),
-    );
-  }
 }
